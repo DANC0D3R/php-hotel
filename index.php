4159059950c5cd11b16bc4ba57f094b9d29b448d
Daniele Minieri <danieleminieri99@gmail.com>
2023-02-20 14:21:20 +0100

    Form completato (versione non funzionante)

--- index.php
+++ index.php
@@ -36,6 +36,11 @@
             'distance_to_center' => 50
         ],
     ];
+    $park = $_GET['parcheggio'];
+    $rating = $_GET['voto'];
+    var_dump($park);
+    var_dump(empty($park));
+    var_dump($rating);
 ?>
 
 <!DOCTYPE html>
@@ -78,12 +83,37 @@
 
                 margin: 20px auto;
             }
+            form>div{
+                width:70%;
+            }
+            .dc-sel, .dc-btn, .dc-num{
+                width:18%;
+
+                margin:0 18px;
+            }
         </style>
     </head>
 
     <body>
         <h1>Scegli il tuo Hotel</h1>
-
+        <!-- Form -->
+        <form action="./index.php" method="GET">
+            <div class="m-auto d-flex justify-content-center">
+                <!-- Parcheggio -->
+                <select name="parcheggio" class="form-select dc-sel" aria-label="Default select example">
+                    <option value="" selected>Parcheggio Int.</option>
+                    <option value="1">Sì</option>
+                    <option value="0">No</option>
+                </select>
+                <!-- Voto -->
+                <input type="number" name="voto" class="form-control dc-num" id="exampleFormControlInput1" placeholder="Voto min">
+                <!-- Bottone Submit -->
+                <button class="btn btn-info dc-btn" type="submit">Invia</button>
+                <!-- Bottone Reset -->
+                <input class="btn btn-warning dc-btn" type="reset" value="Reset">
+            </div>
+        </form>
+        <!-- Tabella -->
         <table class="table table-striped dc-table">
         <thead>
             <tr>
@@ -94,14 +124,29 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach($hotels as $hotel):?>
-                <tr>
-                <th scope="row"><?php echo $hotel['name'] ?></th>
-                <td><?php echo ($hotel['parking'])? 'Sì' : 'No';?></td>
-                <td><?php echo $hotel['vote'] ?></td>
-                <td><?php echo $hotel['distance_to_center'] ?></td>
-                </tr>
-            <?php endforeach; ?>
+            <!-- Se non vengono impostati parametri -->
+            <?php if((empty($park) && empty($rating))){ ;?>
+                <?php foreach($hotels as $hotel):?>
+                    <tr>
+                    <th scope="row"><?php echo $hotel['name']; ?></th>
+                    <td><?php echo ($hotel['parking'])? 'Sì' : 'No';?></td>
+                    <td><?php echo $hotel['vote'] ?></td>
+                    <td><?php echo $hotel['distance_to_center'] ;?></td>
+                    </tr>
+                <?php endforeach; ?>
+            
+            <?php }
+            // Altrimenti filtra la ricerca con parametri inseriti
+            else { ?>
+                <?php foreach($hotels as $key => $value){?>
+                    <?php if($value['parking'] == $park && $value['vote']>=$rating) {?>
+                        <tr>
+                            <th scope="row"><?php echo $hotel['name'] ;?></th>
+                            <td><?php echo ($hotel['parking']);?></td>
+                            <td><?php echo $hotel['vote'] ;?></td>
+                            <td><?php echo $hotel['distance_to_center'] ;?></td>
+                        </tr>
+            <?php } } } ?>
         </tbody>
     </body>
 </html>
