ca74b109f8f9af3ea29f2aaf681c43f9a522866b
Daniele Minieri <danieleminieri99@gmail.com>
2023-02-20 13:52:15 +0100

    Layout a tabella completato

--- index.php
+++ index.php
@@ -46,17 +46,62 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Daniele Minieri">
         <title>PHP Hotels</title>
+
+        <!-- Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Phudu:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
+        <!-- Stile -->
+        <style>
+            body{
+                font-family: 'Phudu', cursive;
+            }
+            h1{
+                text-align: center;
+
+                margin: 20px auto;
+
+                color: #FF385C;
+            }
+            th[scope="col"]{
+                width: calc(100% / 4);
+
+                font-weight: bold;
+
+                color: #FF385C;
+            }
+            .dc-table{
+                width: 70%;
+
+                margin: 20px auto;
+            }
+        </style>
     </head>
 
     <body>
-        <div>
-            <?php foreach($hotels as $hotel) : ?>
-                <p>
-                    <?php foreach($hotel as $key => $value) : ?>
-                    <b><?= $key ?></b>: <?= $value ?>  
-                    <?php endforeach; ?>
-                </p>
+        <h1>Scegli il tuo Hotel</h1>
+
+        <table class="table table-striped dc-table">
+        <thead>
+            <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Parcheggio Interno</th>
+            <th scope="col">Voto</th>
+            <th scope="col">Distanza dal centro (km)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($hotels as $hotel):?>
+                <tr>
+                <th scope="row"><?php echo $hotel['name'] ?></th>
+                <td><?php echo ($hotel['parking'])? 'Sì' : 'No';?></td>
+                <td><?php echo $hotel['vote'] ?></td>
+                <td><?php echo $hotel['distance_to_center'] ?></td>
+                </tr>
             <?php endforeach; ?>
-        </div>
+        </tbody>
     </body>
 </html>
